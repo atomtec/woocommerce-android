846e1295220ab64163008a3846735c3d6844bac5
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2022-01-17 16:36:44 -0700

    Add dependencies

--- .buildkite/commands/checkstyle.sh
+++ .buildkite/commands/checkstyle.sh
@@ -3,25 +3,14 @@
 echo "--- ⤵️ Installing Bash Cache Plugin"
 cp -v /bash-cache/bin/* /usr/bin
 
-echo "---"
-echo $BUILDKITE_PIPELINE_SLUG
-echo $BUILDKITE_LABEL
-echo "---"
+apt install -y awscli jq
 
-CACHEKEY="${BUILDKITE_PIPELINE_SLUG}-${BUILDKITE_LABEL}"
-
-echo "---"
-echo $CACHEKEY
-echo $CACHE_BUCKET_NAME
-echo "---"
 
 echo "--- 🧹 Linting"
-
+CACHEKEY="${BUILDKITE_PIPELINE_SLUG}-${BUILDKITE_LABEL}"
 restore_cache "$CACHEKEY"
 
 cp -v gradle.properties-example gradle.properties
 ./gradlew checkstyle
 
 save_cache /root/.android $CACHEKEY
-ls /root
-ls /root/.android
