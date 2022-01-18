d97a83b5f6ea963fab4ea8709f5174e3afc0c222
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2022-01-17 17:03:20 -0700

    Fix cache restoration

--- .buildkite/commands/checkstyle.sh
+++ .buildkite/commands/checkstyle.sh
@@ -5,10 +5,11 @@ cp -v /bash-cache/bin/* /usr/bin
 
 apt install -y awscli jq
 
-
 echo "--- 🧹 Linting"
 CACHEKEY="${BUILDKITE_PIPELINE_SLUG}-${BUILDKITE_LABEL}"
 restore_cache "$CACHEKEY"
+
+ls -la /root
 
 cp -v gradle.properties-example gradle.properties
 ./gradlew checkstyle
